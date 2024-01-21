c4f23fda2331b6ecbf1b2bb58ad171a9de66c3f6
MellowMokoen <mamello3632@gmail.com>
2024-01-21 14:26:41 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+const App = () => {
+  return <div>
+    <section className='h-screen bg-red-500'>
+       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam iusto consequuntur modi facere quas magnam unde recusandae dolorem iure voluptates repudiandae, odit eos vitae tempore quaerat quis architecto quam sunt!</p>
+    </section>
+  </div>;
+
+};
+
+export default App
