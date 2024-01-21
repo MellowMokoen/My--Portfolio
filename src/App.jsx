dfc98c015256dcd47fb336315316672c05504e7a
MellowMokoen <mamello3632@gmail.com>
2024-01-21 18:02:45 +0200

    Created sidenav comp. and used tailwind for style

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import './App.css'
+import React from "react";
+import Sidenav from './components/Sidenav';
 
-const App = () => {
-  return <div>
-    <section className='h-screen bg-red-500'>
-       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam iusto consequuntur modi facere quas magnam unde recusandae dolorem iure voluptates repudiandae, odit eos vitae tempore quaerat quis architecto quam sunt!</p>
-    </section>
-  </div>;
+function App() {
+  return (
+    <div>
+      <Sidenav />
+    </div>
+
+  )
 
 };
 
